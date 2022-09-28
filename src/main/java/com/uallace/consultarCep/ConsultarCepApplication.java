ac77e6aa79c436960c8879c82d8a232873b2d22e
Uallace Gomes <uallace.gomes.silva@gmail.com>
2022-09-28 02:05:44 -0300

    adicionando arquivos do projeto

---
+++ src/main/java/com/uallace/consultarCep/ConsultarCepApplication.java
@@ -0,0 +1,13 @@
+package com.uallace.consultarCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultarCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultarCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uallace/consultarCep/ConsultarCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uallace.consultarCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultarCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
